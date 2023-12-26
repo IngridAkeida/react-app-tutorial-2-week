0a3e14c17e9b6d46cac3d817bcb6b3913171c47d
IngridAkeida <ingridsanchesp@gmail.com>
2023-12-26 18:05:44 +0100

    add - props

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = [
-    'New York',
-    'San Francisco',
-    'Tokyo',
-    'London',
-    'Paris'
-  ];
+//{items: [], heading: string}
+
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({items, heading}: Props) {
 
   //Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
@@ -17,7 +17,7 @@ function ListGroup() {
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {message}
       <ul className="list-group">
         {items.map((item, index) => (
